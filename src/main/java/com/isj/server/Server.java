d30d8d8266178284f9e933c0e359f31aa59b2c52
Indecis1 <sopalwyn@gmail.com>
2021-12-19 16:36:39 +0100

    Change port in server class

--- src/main/java/com/isj/server/Server.java
+++ src/main/java/com/isj/server/Server.java
@@ -10,19 +10,27 @@ public class Server {
     public Server(){}
 
     public static void main(String args[]){
-        String serverAddress = "172.16.10.144";
+        String port = System.getenv("PORT");
+        int PORT;
         try {
             // Cree l'objet distant
             OperationImpl obj = new OperationImpl();
 
-            // Ici, nous exportons l'objet distant vers le stub
-            Operation stub = (Operation) UnicastRemoteObject.exportObject(obj, 0);
 
-            // Liaison de l'objet distant (stub) dans le Registre
-            Registry reg = LocateRegistry.createRegistry(8080);
-            reg.bind("Operation", stub);
-            System.out.println("Le serveur est prêt...");
 
+            try{
+                PORT = Integer.parseInt(port);
+
+                // Ici, nous exportons l'objet distant vers le stub
+                Operation stub = (Operation) UnicastRemoteObject.exportObject(obj, PORT);
+
+                // Liaison de l'objet distant (stub) dans le Registre
+                Registry reg = LocateRegistry.createRegistry(PORT);
+                reg.bind("Operation", stub);
+                System.out.println("Le serveur est prêt...");
+            }catch (Exception e){
+                System.out.println("Le port doit être un entier");
+            }
         } catch(Exception e){
             System.err.println(e.toString());
             e.printStackTrace();
